211ef2986afb40a26c233bef073020d6e46058aa
GabrielMBatista <gabbriel_gbl2@hotmail.com>
2020-12-18 11:44:11 -0300

    inicio enviar msg

---
+++ whatslab/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import EnvioMsg from './components/EnvioMsg';
+
+function App() {
+  return (
+    <div className="App">
+
+      <EnvioMsg />
+
+    </div>
+  );
+}
+
+export default App;
