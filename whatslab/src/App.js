06200dc85374cf59b4c255053261e6e6463a5fbd
cptvictor95 <cpt.victor@hotmail.com>
2020-12-18 16:18:44 -0300

    whatslab completo, desafio 1 feito

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,15 +1,101 @@
 import EnvioMsg from "./components/EnvioMsg";
 import React from "react";
 
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.section`
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2rem 1rem;
+  height: 100vh;
+`;
+const Aside = styled.aside`
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  padding: 0 2rem;
+  height: 100%;
+`;
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 60vw;
+  padding: 0 2rem;
+  border-right: 1px solid #291f1e;
+  height: 100%;
+  color: #291f1e;
+`;
+
+const Input = styled.input`
+  color: #291f1e;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid #c3d5af;
+  background-color: #dae6d4;
+`;
+const Label = styled.label`
+  padding: 0.5rem 0;
+`;
+const Textarea = styled.input`
+  color: #291f1e;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid #c3d5af;
+  background-color: #dae6d4;
+`;
+
+const FormControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem 0;
+`;
+
+const Button = styled.button`
+  background-color: #f64740;
+  color: #f1f6f8;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: none;
+  width: 5rem;
+  margin: 1rem 0;
+  align-self: flex-end;
+`;
+
+const Header = styled.h1`
+  color: #f64740;
+  background-color: #c3d5af;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15vh;
+`;
+
+const Subtitle = styled.h2``;
+
 class App extends React.Component {
   state = {
     mensagens: [
       {
         createdAt: 0,
-        nomeUsuario: "",
-        mensagemUsuario: "",
+        nomeUsuario: "Labenu",
+        mensagemUsuario: "Olá, essa é uma mensagem teste",
       },
     ],
+  };
+
+  enterButton = (event) => {
+    if (event.key === "Enter") {
+      document.getElementById("submit").click();
+    }
   };
 
   addMensagem = (event) => {
@@ -22,10 +108,18 @@ class App extends React.Component {
 
     const novaLista = [...this.state.mensagens, novaMensagem];
 
-    this.setState({ mensagens: novaLista });
+    if (
+      this.state.inputNomeUsuario === undefined ||
+      this.state.inputMensagemUsuario === undefined ||
+      this.state.inputNomeUsuario === "" ||
+      this.state.inputMensagemUsuario === ""
+    ) {
+      alert("Preencha os campos");
+    } else {
+      this.setState({ mensagens: novaLista });
+    }
 
-    console.log(novaLista);
-    console.log(this.state.mensagens);
+    alert("Mensagem enviada com sucesso!");
     this.setState({
       inputNomeUsuario: "",
       inputMensagemUsuario: "",
@@ -50,22 +144,33 @@ class App extends React.Component {
       ));
     });
     return (
-      <div className="App">
-        {listaMsgs}
-        <label>Nome:</label>
-        <input
-          value={this.state.inputNomeUsuario}
-          onChange={this.onChangeNomeUser}
-        />
-        <br />
-        <label>Mensagem:</label>
-        <input
-          value={this.state.inputMensagemUsuario}
-          onChange={this.onChangeMensagem}
-        />
-        <br />
-        <button onClick={this.addMensagem}> Enviar </button>
-      </div>
+      <Main>
+        <Header>WhatsLab</Header>
+        <Container>
+          <Section>{listaMsgs}</Section>
+          <Aside>
+            <Subtitle>Crie sua mensagem</Subtitle>
+            <FormControl>
+              <Label>Nome:</Label>
+              <Input
+                value={this.state.inputNomeUsuario}
+                onChange={this.onChangeNomeUser}
+              />
+            </FormControl>
+            <FormControl>
+              <Label>Mensagem:</Label>
+              <Textarea
+                value={this.state.inputMensagemUsuario}
+                onChange={this.onChangeMensagem}
+                onKeyPress={(e) => this.enterButton(e)}
+              />
+            </FormControl>
+            <Button id="submit" onClick={this.addMensagem}>
+              Enviar
+            </Button>
+          </Aside>
+        </Container>
+      </Main>
     );
   }
 }
